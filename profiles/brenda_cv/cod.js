eaea6f53ddb8377e7999210aa0c0ad47ec9eae1a
Breend <91857905+Breend21@users.noreply.github.com>
2024-02-20 17:40:59 -0600

    Agragar cv de brenda

---
+++ profiles/brenda_cv/cod.js
@@ -0,0 +1,22 @@
+function saludo() {
+    alert("Hola");
+}
+
+saludo();
+
+function fondoParrafos() {
+    var parrafos = document.getElementsByTagName("p");
+    parrafos[0].innerHTML = "Reemplazado";
+    parrafos[1].style.color = "red";
+    var lista = document.getElementsByClassName("azul");
+    lista[0].style.cssText = "text-align:center; color:blue";
+    document.getElementById("raro").innerHTML = "CONTENIDO ACTUAL";
+}
+
+function cambiarEstilo(archivoCSS) {
+    document.getElementById("estilo1").href = archivoCSS;
+    document.getElementById("estilo2").href = archivoCSS;
+    document.getElementById("estilo3").href = archivoCSS;
+    console.log()
+}
+

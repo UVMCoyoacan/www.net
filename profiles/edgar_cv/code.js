a5b04d3430a45558e31d24c81524d5a2e9f16d2d
Computo <computo@my.uvm.edu.mx>
2024-02-20 17:21:25 -0600

    CV de Edgar

---
+++ profiles/edgar_cv/code.js
@@ -0,0 +1,13 @@
+function aviso(){
+    alert("UVM ESTUDIANTES");
+}
+aviso()
+
+function fondoParrafo(){
+    parrafos=document.getElementsByTagName("p");
+    parrafos[0].innerHTML="REMPLAZADO";
+    parrafos[1].style="color:red"
+    lista=document.getElementsByClassName("verde");
+    lsita[0].style="text-align:center; color=blue";
+    document.getElementById("raro").innerHTML="CONTENIDO ACTUAL";
+}

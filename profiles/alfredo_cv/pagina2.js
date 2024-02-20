87b0abeefef1c7be2d85e00b126706e47b29f654
Alfredo <a040091365@my.uvm.edu.mx>
2024-02-20 17:33:23 -0600

    agregando cv de alfredo

---
+++ profiles/alfredo_cv/pagina2.js
@@ -0,0 +1,30 @@
+// Función para cambiar los colores con el botón 1
+document.getElementById("cambiar-colores-btn1").onclick = function() {
+    cambiarEstilos(1);
+};
+
+// Función para cambiar los colores con el botón 2
+document.getElementById("cambiar-colores-btn2").onclick = function() {
+    cambiarEstilos(2);
+};
+
+// Función para cambiar los colores con el botón 3
+document.getElementById("cambiar-colores-btn3").onclick = function() {
+    cambiarEstilos(3);
+};
+
+// Función para cambiar los estilos de la página
+function cambiarEstilos(numeroEstilo) {
+    // Ocultar todos los estilos CSS
+    for (let i = 1; i <= 3; i++) {
+        document.getElementById(`stylesheet${i}`).disabled = true;
+    }
+
+    // Activar el estilo correspondiente
+    document.getElementById(`stylesheet${numeroEstilo}`).disabled = false;
+}
+
+document.getElementById("botonRegistro").onclick = function() {
+    window.location.href = "formulario.html";
+};
+
